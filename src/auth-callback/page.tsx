e29c659e2659f20f0b5a6fef3e724729ad13f4b5
Lotte Kvalheim <lottek@stud.ntnu.no>
2025-01-07 14:22:29 +0100

    added trpc functionality

---
+++ src/auth-callback/page.tsx
@@ -0,0 +1,22 @@
+import { trpc } from "@/app/_trpc/client"
+import { useRouter, useSearchParams } from "next/navigation"
+
+const Page = async () => {
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+  const origin = searchParams.get("origin")
+
+  const{data, isLoading} = trpc.authCallback.useQuery()
+
+  // Use useEffect to handle redirect after successful authentication
+  useEffect(() => {
+    if (data?.success) {
+      router.push(origin ? `/${origin}` : "/dashboard")
+    }
+  }, [data, origin, router]) // Dependencies: re-run when `data` or `origin` changes
+
+  return isLoading ? <p>Loading...</p> : <p>Redirecting...</p>
+
+}
+export default Page
---
+++ src/trpc/index.ts
@@ -0,0 +1,16 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { publicProcedure, router } from "./trpc"
+import { TRPCError } from "@trpc/server"
+export const appRouter = router({
+  authCallback: publicProcedure.query(async () =>{
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
+    if(!(await user).id || !(await user).email) 
+      throw new TRPCError({code: "UNAUTHORIZED"})
+    // check if the user is in the database
+    return {success: true}
+  })
+})
+
+export type AppRouter = typeof appRouter
